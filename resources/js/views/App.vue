3cef1d7806fa0448d752651f3d9006663e654d72
Chloé Renault <chloerenault@macbook-pro-chloe.home>
2021-08-16 15:09:21 +0200

    vue/tailwindcss install

---
+++ resources/js/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p class="font-black text-gray-100 bg-black">{{message}}</p>
+  </div>
+</template>
+<script>
+const default_layout = "default";
+
+
+export default {
+  computed: {},
+  data() {
+      return {
+          message:'Hello World'
+      }
+  }
+};
+</script>
